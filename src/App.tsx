c3d0fcc1d862128c174b9bc313a2450122d65ac3
ish1zuecounseling-oss <ish1zue.counseling@gmail.com>
2026-03-12 21:25:15 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import HelperBurnout from "./pages/HelperBurnout"
+import EmotionalLabor from "./pages/EmotionalLabor"
+import HelperBoundary from "./pages/HelperBoundary"
 
-export default function App() {
+function App() {
   return (
-
     <BrowserRouter>
-
       <Routes>
-
         <Route path="/" element={<Home />} />
-
         <Route path="/helper-burnout" element={<HelperBurnout />} />
-
+        <Route path="/emotional-labor" element={<EmotionalLabor />} />
+        <Route path="/helper-boundary" element={<HelperBoundary />} />
       </Routes>
-
     </BrowserRouter>
-
   )
 }
+
+export default App
