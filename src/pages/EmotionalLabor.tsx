83447b93af93e30f31d2613006c64e0a5a9e5d58
ish1zuecounseling-oss <ish1zue.counseling@gmail.com>
2026-03-16 01:06:00 +0900

    Update EmotionalLabor.tsx

--- src/pages/EmotionalLabor.tsx
+++ src/pages/EmotionalLabor.tsx
@@ -5,6 +5,7 @@
 
 import { motion } from "motion/react";
 import RelatedArticles from "../components/RelatedArticles"
+import AudioPlayer from "../components/AudioPlayer"
 
 export default function App() {
   return (
