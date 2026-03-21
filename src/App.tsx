f6b432948eb807748779eab2fc93dc7528790919
ish1zuecounseling-oss <ish1zue.counseling@gmail.com>
2026-03-21 23:01:01 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./components/Layout"
-import Home from "./pages/Home"
-import Articles from "./pages/Articles"
-import Profile from "./pages/Profile"
+const articleRoutes = Object.entries(articlePages)
+  .map(([path, module]: any) => {
+    const name = path
+      .replace("./pages/Articles/", "")
+      .replace(".tsx", "")
+      .replace(/([A-Z])/g, "-$1")
+      .toLowerCase()
+      .replace(/^-/, "")
 
-const articlePages = import.meta.glob("./pages/Articles/*.tsx", { eager: true })
+    const Component = module.default
 
-function App() {
-
-  const articleRoutes = Object.entries(articlePages)
-    .filter(([path]) => !path.includes("Home") && !path.includes("Articles") && !path.includes("Profile"))
-    .map(([path, module]: any) => {
-      const name = path
-
-  .replace("./pages/Articles/", "")
-  .replace(".tsx", "")
-  .replace(/([A-Z])/g, "-$1")
-  .toLowerCase()
-  .replace(/^-/, "")
-
-      const Component = module.default
-
-      return (
-        <Route
-          key={name}
-          path={`/articles/${name}`}
-          element={<Component />}
-        />
-      )
-    })
-
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-
-          <Route path="/" element={<Home />} />
-          <Route path="/articles" element={<Articles />} />
-          <Route path="/profile" element={<Profile />} />
-
-          {articleRoutes}
-
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  )
-}
-
-export default App
+    return (
+      <Route
+        key={name}
+        path={`/articles/${name}`}
+        element={<Component />}
+      />
+    )
+  })
