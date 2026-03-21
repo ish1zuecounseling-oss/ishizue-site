12fe23f3f274ef9eb4869a0c455113bd3fbdb24a
ish1zuecounseling-oss <ish1zue.counseling@gmail.com>
2026-03-21 22:38:00 +0900

    Update HelperCounseling.tsx

--- src/pages/Articles/HelperCounseling.tsx
+++ src/pages/Articles/HelperCounseling.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import RelatedArticles from "../components/RelatedArticles";
+import RelatedArticles from "../../components/RelatedArticles"
 import AudioPlayer from "../components/AudioPlayer";
 import ArticleSchema from "../components/ArticleSchema";
 import Breadcrumbs from "../components/Breadcrumbs";
