02435ed37ead1181c18101b03c45ea43fef786f9
ish1zuecounseling-oss <ish1zue.counseling@gmail.com>
2026-03-28 00:47:15 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { HelmetProvider } from "react-helmet-async";
+
+<HelmetProvider>
+  <App />
+</HelmetProvider>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
