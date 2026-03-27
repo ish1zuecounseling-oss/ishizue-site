5ad7aff784ab37c4a5285bfcec26542a8ba4254c
ish1zuecounseling-oss <ish1zue.counseling@gmail.com>
2026-03-27 23:33:52 +0900

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -315,7 +315,6 @@ const Home: React.FC = React.memo(() => {
             </motion.div>
           </div>
         </section>
-
         {/* SEO導線 */}
         <section className="py-10 px-6 bg-white border-b border-stone-100">
           <div className="max-w-3xl mx-auto">
