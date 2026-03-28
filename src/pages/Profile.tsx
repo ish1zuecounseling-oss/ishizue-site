4fa465ae59f69b94d12fdb1d2bd4375db9c047be
ish1zuecounseling-oss <ish1zue.counseling@gmail.com>
2026-03-28 11:23:04 +0900

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import AudioPlayer from "../components/AudioPlayer";
 import { Link } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
