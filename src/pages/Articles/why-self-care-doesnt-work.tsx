2ee30cf5582fef06dd34a97ff24a7db5896bb755
ish1zuecounseling-oss <ish1zue.counseling@gmail.com>
2026-05-13 08:46:40 +0900

    Update why-self-care-doesnt-work.tsx

--- src/pages/Articles/why-self-care-doesnt-work.tsx
+++ src/pages/Articles/why-self-care-doesnt-work.tsx
@@ -1,6 +1,6 @@
 import ArticleLayout from "../../components/ArticleLayout"
 import { Link } from "react-router-dom"
-import { LineCtaSmall } from "../../components/LineCta"
+import { LineCtaFatigue } from "../../components/LineCta"
 import ArticleFooterLinks from "../../components/ArticleFooterLinks"
 
 export default function WhySelfCareDoesntWork() {
@@ -73,7 +73,7 @@ export default function WhySelfCareDoesntWork() {
         今の消耗の深さを確認→ <Link to="/articles/helper-empathy-check" className="underline underline-offset-2 text-stone-600 hover:text-stone-900">共感疲労チェック（20項目）</Link>
       </p>
 
-      <LineCtaSmall />
+      <LineCtaFatigue />
 
       <h2>セルフケアが「効く」ようになるために</h2>
 
